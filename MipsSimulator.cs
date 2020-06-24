76d22824c495c00734e310369b06356c842915c4
MarceloHeredia <heredia_m@outlook.com>
2020-06-23 23:47:28 -0300

    First Commit

---
+++ MipsSimulator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SimuladorMIPS
+{
+    class MipsSimulator
+    {
+        static void Main(string[] args)
+        {
+            var x = 0x40404004;
+            Int32 y = 0b111;
+            Console.WriteLine(y);
+
+            int a = 10;
+
+            var bin = Convert.ToString(a, 2);
+            if (a > 0) { bin = bin.PadLeft(32, '0'); }
+
+
+            Console.WriteLine(bin);
+
+            int b = Convert.ToInt32(bin, 2);
+
+            Console.WriteLine(b);
+
+
+            Console.ReadKey();
+        }
+    }
+}
